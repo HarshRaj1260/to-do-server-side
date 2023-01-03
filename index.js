c22d062a0ec9f2f2fb1a693355ab9f459ce0553e
Harsh Raj <2019kuec2045@iiitkota.ac.in>
2023-01-03 20:07:37 +0530

    corrected the path

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use(express.urlencoded({ extended: true }));
 //     });
 // }
 
-app.use("/api", apiRouter);
+app.use("./api", apiRouter);
 app.use(express.static("public"));
 app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
